b8043d70dc61399ddd8c922cfe44176c159bb9e9
SeanLovesTea <71432368+SeanLovesTea@users.noreply.github.com>
2021-09-24 18:28:44 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -13,81 +13,55 @@ function compChoiceF(){
 let userChoice= userChoiceF();
 let compChoice= compChoiceF();
 
-function compareChoice(userChoice, compChoice){
-   
+function playRound(userChoice, compChoice){
+    
 
     if(userChoice === compChoice){
         return "That was a draw!";
     }
     else if(userChoice === "rock" && compChoice === "scissors"){
-            playerLives++ ;
+            playerScore++ ;
             return "Rock beats Scissors, you win!";
         }
         else if(userChoice ="rock" && compChoice ==="paper"){
-            computerLives++ ;
+            computerScore++ ;
             return "Paper beats Rock, you lose!" ;
          }
     else if(userChoice === "paper" && compChoice === "scissors"){
-            computerLives++;
+            computerScore++;
             return "Scissors beats Paper, you lose!" ;
         }
         else if (userChoice = "paper" && compChoice === "rock"){
-            playerLives++;
+            playerScore++;
             return "Paper beats Rock, you win!";
     }
     else if(userChoice === "scissors" && compChoice === "paper"){
-            playerLives++;
+            playerScore++;
             return "Scissors beats Paper, you win!" ;
         }
         else if(userChoice === "scissors" && compChoice === "rock"){
-            computerLives++;
+            computerScore++;
             return "Rock beats scissors, you lose!" ;
         }
     }
-let playerLives = 0;
-let computerLives = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-console.log("The computer chose " +compChoice);
-console.log("You chose " +userChoice);
-console.log(compareChoice(userChoice, compChoice));
-console.log("You have  "+playerLives , " The computer has " +computerLives);
+for ( i=0; i<5; i++){
+
+    let userChoice= userChoiceF();
+    let compChoice= compChoiceF();
+
+    console.log("The computer chose " +compChoice);
+    console.log("You chose " +userChoice);
+    console.log(playRound(userChoice, compChoice));
+    console.log("You have  "+playerScore , " The computer has " +computerScore);
+}
 
 
-///console.log("the computer chose " +compChoice);
-//console.log("You chose " +userChoice);
-//console.log(compareChoice(userChoice, compChoice));
-//console.log("You have  "+playerLives , " The computer has " +computerLives);
 
 
-//while ( playerLives < 3 && computerLives < 3){
-   // console.log(userChoice);
-   /// console.log(compChoice);
-    
-//}
-//}   
-//while ( playerLives = 1 , computerLives = 1){
-    //console.log(userChoiceF());
-   //console.log(compChoiceF());
-   // console.log("the computer chose " +compChoice);
-    //console.log("You chose " +userChoice);
-    //console.log(compareChoice(userChoice, compChoice));
-   // console.log("You have  "+playerLives , " The computer has " +computerLives);
-//}
-/////if (playerLives =3, computerLives =3){
-    //throw new error(" YOu have finished the game");
-//}
-//if(i = 0, i>4 , i++){
- ////   console.log(compareChoice(userChoice, compChoice));
-//}
 
-//function playGame(){
-    //compareChoice(userChoice, compChoice);
-//}
-//function playRounds(){
-   // if(i=0, i >=4, i++){
-    //    playGame();
-    //}
-//}
 
 
 
